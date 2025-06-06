15cc93034775336f3e5dd5eae65d0f19662007a6
fechilli <luizfdas@outlook.com>
2025-06-05 22:04:48 -0300

    inicio da migration para finalizar a tabela

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Lembretes
 
 Sistema de lembretes com opção de alerta
+
+Caso for clonar o codigo deve seguir esses passos
+
+1- baixar NODE.JS caso não tenha instalado na máquina
+2- no terminal, acessar o diretório onde está salvo o programa e usar o comando "npm install"
+3- usar o comando "npm init" para iniciar o projeto node.
+4- usar comando "npm install sequelize mysql2" para instalar dependencias do banco de dados (Se estiver usando outro banco, troque mysql2 pelo driver correspondente.)
+5- usar comando "npm install --save-dev sequelize-cli" para manipular os models do banco de dados
+6= usar comando "npm install nodemon" para rodar o script
--- databases/database.js
+++ databases/database.js
@@ -1,5 +1,23 @@
 // passo 1 criando conexão com banco de dados
 
+require('dotenv').config(); // Para carregar variáveis de ambiente do .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'sua_senha_mysql', // Mude para sua senha do MySQL
+    database: process.env.DB_DATABASE || 'nome_do_seu_banco', // Mude para o nome do seu banco de dados
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+    logging: false // Defina como true para ver os logs SQL no console
+  },
+  // Você pode ter outras configurações para produção, teste, etc.
+  production: {
+    // ...
+  }
+};
+
 const { Sequelize } = require('sequelize'); // importar sequelize
 
 const sequelize = new Sequelize('tarefas', 'root', '', {
